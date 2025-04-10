81b4023924358766fab1f89a5f1fea99508dcd32
Hokyagency <ubuntu@GCFRBO024.GAMINGCAMPUS.local>
2025-04-10 17:21:10 +0200

    add build/serial main

---
+++ build/CMakeFiles/STMcontroler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STMcontroler.dir/main.c.o"
+  "CMakeFiles/STMcontroler.dir/main.c.o.d"
+  "STMcontroler"
+  "STMcontroler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/STMcontroler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
